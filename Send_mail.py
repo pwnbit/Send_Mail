8b4bd3f55e05768a2214bb1485de342ac8624b46
pwnbit <pwnbit@gmail.com>
2016-06-20 05:19:24 +0900

    시간 기능 추가 및 버그 수정
    코드 실행 시간 추가 및 변수명 버그 수정

--- Send_mail.py
+++ Send_mail.py
@@ -1,19 +1,27 @@
+import smtplib, socket
 from email.mime.text import MIMEText
+from datetime import datetime
 
-def sendMail(ip):
+def sendMail(ip, time):
     host = 'smtp.naver.com'
-    contents = ip
+    sender = 'sender_mail'
+    receiver = 'receiver_mail'
+    contents = str(time) + '-' + ip
     ID = ''
     PW = ''
 
     msg = MIMEText(contents, _charset='euc-kr')
     msg['Subject'] = 'Windows Login - ' + ip
+    msg['From'] = sender
+    msg['To'] = receiver
 
     s = smtplib.SMTP_SSL(host, 465)
     s.login(ID, PW)
-    s.sendmail(me, you, msg.as_string())
+    s.sendmail(sender, receiver, msg.as_string())
     s.quit()
 
 get_ip = socket.gethostbyname(socket.gethostname())
+get_time = datetime.now()
+get_time = get_time.strftime("%Y.%m.%d %H:%M:%S")
 
-sendMail(get_ip)
+sendMail(get_ip, get_time)
